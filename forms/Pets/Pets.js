934059800f156991a27a3a5f3b12c86729d5d54e
gak77901 <gak77901@creighton.edu>
2021-09-15 16:17:32 -0500

    Finished LA

---
+++ forms/Pets/Pets.js
@@ -0,0 +1,15 @@
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+
+let withSal = petNames
+withSal.push('Sal')
+console.log(withSal)
+
+console.log(`The third pet is named ${petNames[2]}`)
+
+console.log(`The array has ${withSal.length} names in it.`)
+
+withSal.pop()
+console.log(withSal)
+
+
+
